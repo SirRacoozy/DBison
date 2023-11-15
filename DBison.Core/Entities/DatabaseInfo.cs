f01b23e28fc671ab60eeb0e30d94ce293b442bc2
SirCodiac <118847092+Secodity@users.noreply.github.com>
2023-11-15 17:31:33 +0100

    Added database info and database connection manager

---
+++ DBison.Core/Entities/DatabaseInfo.cs
@@ -0,0 +1,58 @@
+﻿namespace DBison.Core.Entities;
+public class DatabaseInfo
+{
+    #region - properties -
+
+    #region [Name]
+    public string Name { get; set; } = string.Empty;
+    #endregion
+
+    #region [Server]
+    public string Server {  get; set; } = string.Empty;
+    #endregion
+
+    #region [Username]
+    public string Username { get; set; } = string.Empty;
+    #endregion
+
+    #region [Password]
+    public string Password { internal get; set; } = string.Empty;
+    #endregion
+
+    #region [UseIntegratedSecurity]
+    public bool UseIntegratedSecurity { get; set; } = false;
+    #endregion
+
+    #endregion
+
+    #region - methods -
+
+    #region - override -
+
+    #region [ToString]
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Server)}: {Server}, {nameof(Username)}: {Username}, {nameof(UseIntegratedSecurity)}: {UseIntegratedSecurity}";
+    }
+    #endregion
+
+    #region [GetHashCode]
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Server, Username, UseIntegratedSecurity);
+    }
+    #endregion
+
+    #region [Equals]
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || !(obj is DatabaseInfo))
+            return false;
+        return GetHashCode() == (obj as DatabaseInfo)!.GetHashCode();
+    }
+    #endregion
+
+    #endregion
+
+    #endregion
+}
---
+++ DBison.Core/Helper/DatabaseConnectionManager.cs
@@ -0,0 +1,94 @@
+﻿using DBison.Core.Entities;
+using Microsoft.Data.SqlClient;
+using System.Collections.Concurrent;
+using System.Data;
+using System.Linq;
+
+namespace DBison.Core.Helper;
+public class DatabaseConnectionManager
+{
+
+	#region - ctor -
+	internal DatabaseConnectionManager()
+	{
+		m_Connections = new();
+	}
+	#endregion
+
+	#region - properties -
+
+	#region [Instance]
+	public static DatabaseConnectionManager Instance = m_Manager ?? new();
+	#endregion
+
+	#region [m_Manager]
+	private static DatabaseConnectionManager? m_Manager { get; set; }
+	#endregion
+
+	#region [m_Connections]
+	private ConcurrentDictionary<DatabaseInfo, SqlConnection> m_Connections { get; set; }
+	#endregion
+
+	#endregion
+
+	#region - methods -
+
+	#region [AddOrGetConnection]
+	public SqlConnection AddOrGetConnection(DatabaseInfo databaseInfo)
+	{
+		if(m_Connections.ContainsKey(databaseInfo))
+		{
+			if (m_Connections.TryGetValue(databaseInfo, out var connection))
+				return connection;
+			connection = __CreateConnection(databaseInfo);
+			m_Connections[databaseInfo] = connection;
+			return connection;
+		}
+
+		var conn = __CreateConnection(databaseInfo);
+		m_Connections.TryAdd(databaseInfo, conn);
+		return conn;
+	}
+	#endregion
+
+	#region [CloseConnection]
+	public void CloseConnection(SqlConnection connection)
+	{
+		if (connection is null) throw new ArgumentNullException(nameof(connection));
+		if (connection.State != ConnectionState.Closed)
+			connection.Close();
+		var connectionsToRemove = m_Connections.Where(x => x.Value.Equals(connection));
+		foreach (var conn in connectionsToRemove)
+			m_Connections.TryRemove(conn);
+	}
+	#endregion
+
+	#region [CloseConnection]
+	public void CloseConnection(DatabaseInfo databaseInfo)
+	{
+        ArgumentNullException.ThrowIfNull(databaseInfo);
+        if (m_Connections.ContainsKey(databaseInfo))
+			CloseConnection(m_Connections[databaseInfo]);
+	}
+	#endregion
+
+	#region [__CreateConnection]
+	private SqlConnection __CreateConnection(DatabaseInfo databaseInfo)
+	{
+		var builder = new SqlConnectionStringBuilder()
+		{
+			DataSource = databaseInfo.Server,
+			InitialCatalog = databaseInfo.Name,
+		};
+		if(!databaseInfo.UseIntegratedSecurity)
+		{
+			builder.UserID = databaseInfo.Username;
+			builder.Password = databaseInfo.Password;
+		}
+		return new SqlConnection(builder.ToString());
+	}
+	#endregion
+
+	#endregion
+
+}
