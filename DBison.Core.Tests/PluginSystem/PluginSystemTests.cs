6475b52e7073f47968f94ebfdc1dd625de6db475
SirCodiac <118847092+Secodity@users.noreply.github.com>
2023-11-18 21:44:11 +0100

    Added test for plugin loader

---
+++ DBison.Core.Tests/PluginSystem/PluginSystemTests.cs
@@ -0,0 +1,22 @@
+﻿using DBison.Core.PluginSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DBison.Core.Tests.PluginSystem;
+
+[TestClass]
+public class PluginSystemTests
+{
+
+    [TestMethod]
+    public void TestOfLoadPlugins()
+    {
+        var path = @"..\..\..\TestData\PluginSystem\";
+        var loader = new PluginLoader(path);
+
+        Assert.AreEqual(1, loader.SearchParsingPlugins.Count, "0x00");
+        Assert.AreEqual(1, loader.ContextMenuPlugins.Count, "0x01");
+        Assert.AreEqual("MyExampleSearchParsingPlugin", loader.SearchParsingPlugins.First().ParseSearchInput(null).Name, "0x02");
+        Assert.AreEqual("MyExampleContextMenuPlugin", loader.ContextMenuPlugins.First().Execute(null).Message, "0x03");
+    }
+
+}
