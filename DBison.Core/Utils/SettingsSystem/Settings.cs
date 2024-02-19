ce79857d0a6a1d8a7851f1e5caf8d3581d343dc9
SirCodiac <43681454+SirCodiac@users.noreply.github.com>
2024-02-19 14:46:15 +0100

    Added `SettingAttribute` class and new settings in `Settings.cs`
    Added a new class `SettingAttribute` in `SettingAttribute.cs` which is a custom attribute for annotating properties with four parameters: `groupName`, `header`, `type`, and `isVisible`. The `System` namespace import in `Settings.cs` was replaced with `DBison.Core.Attributes` to use this new class.
    
    Several new settings were added to the `Settings` class, each annotated with the `SettingAttribute` for additional metadata. These settings include `FontSize`, `UseDarkMode`, `Timeout`, `Limit`, `UseTranslucentWindow`, `TranslucentOpacity`, `UIScaling`, `UseSessionStore`, `MaxBackupCount`, `MaxLogSize`, `ShowExtendedDatabaseInformation`, and `OpenQueryOnServerAdded`.
    
    Some of these settings also have a `Range` attribute applied to specify the valid range of values. For instance, `FontSize` has a range of 10 to 100, and `UIScaling` has a range of 0.1 to 2.0.

--- DBison.Core/Utils/SettingsSystem/Settings.cs
+++ DBison.Core/Utils/SettingsSystem/Settings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DBison.Core.Attributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace DBison.Core.Utils.SettingsSystem;
 
 public static class Settings
 {
+    [Setting("Visual", "Font Size", typeof(uint))]
     [Range(10, 100)]
     public static uint FontSize
     {
@@ -16,12 +18,14 @@ public static class Settings
         set => SettingsHandler.SetSetting("FontSize", value.ToString());
     }
 
+    [Setting("Visual", "Use Dark Mode", typeof(bool))]
     public static bool UseDarkMode
     {
         get => SettingsHandler.GetSetting("UseDarkMode", false);
         set => SettingsHandler.SetSetting("UseDarkMode", value.ToString());
     }
 
+    [Setting("Performance", "Timeout", typeof(int))]
     [Range(-1, int.MaxValue)]
     public static int Timeout
     {
@@ -29,6 +33,7 @@ public static class Settings
         set => SettingsHandler.SetSetting("Timeout", value.ToString());
     }
 
+    [Setting("Performance", "Limit", typeof(int))]
     [Range(-1, int.MaxValue)]
     public static int Limit
     {
@@ -36,12 +41,14 @@ public static class Settings
         set => SettingsHandler.SetSetting("Limit", value.ToString());
     }
 
+    [Setting("Visual", "Use Translucent Window", typeof(bool))]
     public static bool UseTranslucentWindow
     {
         get => SettingsHandler.GetSetting("UseTranslucentWindow", false);
         set => SettingsHandler.SetSetting("UseTranslucentWindow", value.ToString());
     }
 
+    [Setting("Visual", "Translucent Opacity", typeof(uint))]
     [Range(10, 100)]
     public static uint TranslucentOpacity
     {
@@ -49,6 +56,7 @@ public static class Settings
         set => SettingsHandler.SetSetting("TranslucentOpacity", value.ToString());
     }
 
+    [Setting("Visual", "UI Scaling", typeof(double))]
     [Range(0.1, 2.0)]
     public static double UIScaling
     {
@@ -56,12 +64,14 @@ public static class Settings
         set => SettingsHandler.SetSetting("UIScaling", value.ToString());
     }
     
+    [Setting("Performance", "Use Session Store", typeof(bool))]
     public static bool UseSessionStore
     {
         get => SettingsHandler.GetSetting("UseSessionStore", false);
         set => SettingsHandler.SetSetting("UseSessionStore", value.ToString());
     }
 
+    [Setting("Performance", "Max Backup Count", typeof(int))]
     [Range(-1, 100)]
     public static int MaxBackupCount
     {
@@ -69,6 +79,7 @@ public static class Settings
         set => SettingsHandler.SetSetting("MaxBackupCount", value.ToString());
     }
 
+    [Setting("Performance", "Max Log Size", typeof(double))]
     [Range(-1.0, 100.0)]
     public static double MaxLogSize
     {
@@ -76,12 +87,14 @@ public static class Settings
         set => SettingsHandler.SetSetting("MaxLogSize", value.ToString());
     }
 
+    [Setting("Database", "Show Extended Database Information", typeof(bool))]
     public static bool ShowExtendedDatabaseInformation
     {
         get => SettingsHandler.GetSetting("ShowExtendedDatabaseInformation", false);
         set => SettingsHandler.SetSetting("ShowExtendedDatabaseInformation", value.ToString());
     }
 
+    [Setting("Database", "Open Query On Server Added", typeof(bool))]
     public static bool OpenQueryOnServerAdded
     {
         get => SettingsHandler.GetSetting("OpenQueryOnServerAdded", true);
