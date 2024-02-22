589f5875ba4f2f2051306e72a8a085e3a6fe17d9
timo <77628993+timi992000@users.noreply.github.com>
2024-02-22 22:19:29 +0100

    LineNumberedTextBox

---
+++ DBison.WPF/Controls/LineNumberedTextBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DBison.WPF.Controls
+{
+    public class LineNumberedTextBox : TextBox
+    {
+        static LineNumberedTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LineNumberedTextBox), new FrameworkPropertyMetadata(typeof(LineNumberedTextBox)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            __UpdateLineNumber();
+        }
+
+        private void __UpdateLineNumber()
+        {
+            if (GetTemplateChild("PART_LineNumberTextBlock") is TextBlock lineNumberTextBlock)
+                lineNumberTextBlock.Text = string.Join("\n", Enumerable.Range(1, Text.Split('\n').Length));
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+            __UpdateLineNumber();
+        }
+    }
+}
