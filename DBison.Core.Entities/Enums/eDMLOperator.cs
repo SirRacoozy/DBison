98c8fdb7aa0932984f8c244eef52176d454f1085
SirRacoozy <43681454+SirRacoozy@users.noreply.github.com>
2024-03-07 22:07:57 +0100

    Added eDMLOperator enum that is needed for new extender

---
+++ DBison.Core.Entities/Enums/eDMLOperator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBison.Core.Entities.Enums;
+public enum eDMLOperator
+{
+    None,
+    Update,
+    Delete,
+    Insert
+}
