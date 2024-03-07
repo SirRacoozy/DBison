4bdef8134cb018088b87e29395778fa9a6bfba0d
SirRacoozy <43681454+SirRacoozy@users.noreply.github.com>
2024-03-07 22:07:40 +0100

    Added helper to convert to select statement

--- DBison.Core/Extender/StringExtender.cs
+++ DBison.Core/Extender/StringExtender.cs
@@ -1,8 +1,10 @@
-﻿using System.Text;
+﻿using DBison.Core.Entities.Enums;
+using System.Text;
 
 namespace DBison.Core.Extender;
 public static class StringExtender
 {
+    private const StringSplitOptions m_STRINGSPLITTING_NO_EMPTY_TTRIM = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
     public static string ToStringValue(this object rawValue)
     {
         if (rawValue == null)
@@ -43,5 +45,56 @@ public static class StringExtender
     public static bool IsNotNullEmptyOrWhitespace(this string value)
     {
         return !IsNullEmptyOrWhitespace(value);
+    }
+
+    public static (string, eDMLOperator) ConvertToSelectStatement(this string value)
+    {
+        if (value == null)
+            return (string.Empty, eDMLOperator.None);
+
+        if (value.StartsWith("select", StringComparison.InvariantCultureIgnoreCase))
+            return (value, eDMLOperator.None);
+        else if (value.StartsWith("delete", StringComparison.InvariantCultureIgnoreCase))
+            return (__ConvertDeleteToSelectStatement(value), eDMLOperator.Delete);
+        else if (value.StartsWith("update", StringComparison.InvariantCultureIgnoreCase))
+            return (__ConvertUpdateToSelectStatement(value), eDMLOperator.Update);
+
+        return (string.Empty, eDMLOperator.None);
+
+    }
+
+    private static string __ConvertDeleteToSelectStatement(string value)
+    {
+        return value.Replace("delete", "select *", StringComparison.InvariantCultureIgnoreCase);
+    }
+    private static string __ConvertUpdateToSelectStatement(string value)
+    {
+        value = value.ToLower();
+        var splittedBySet = value.Split("set", m_STRINGSPLITTING_NO_EMPTY_TTRIM).ToList();
+
+        var table = splittedBySet.FirstOrDefault()?.Split(' ', m_STRINGSPLITTING_NO_EMPTY_TTRIM).LastOrDefault();
+
+        var splittedByWhere = splittedBySet.LastOrDefault()?.Split("where", m_STRINGSPLITTING_NO_EMPTY_TTRIM).ToList();
+
+        var splittedColumnsAssignments = splittedByWhere.FirstOrDefault()?.Split(",", m_STRINGSPLITTING_NO_EMPTY_TTRIM).ToList();
+
+        var columnsNames = new List<string>();
+
+        splittedColumnsAssignments.ForEach(x => columnsNames.Add(string.Join("", x.Take(x.IndexOf('='))).Trim()));
+
+        StringBuilder sb = new();
+
+        sb.Append("select ");
+        var columns = string.Join(',', columnsNames);
+        if(columnsNames.Count > 1)
+            columns = columns.Take(columns.Length - 1).ToStringValue();
+
+        sb.Append(columns);
+        sb.Append(" from ");
+        sb.Append(table);
+        sb.Append(" where ");
+        sb.Append(splittedByWhere.LastOrDefault() ?? string.Empty);
+
+        return sb.ToString();
     }
 }
