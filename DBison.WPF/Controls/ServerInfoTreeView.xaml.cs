f115d0892ace8323649bb48cd33553d2072aab49
timi992000 <timo992000+git@gmail.com>
2024-03-11 00:59:38 +0100

    Small fixes

--- DBison.WPF/Controls/ServerInfoTreeView.xaml.cs
+++ DBison.WPF/Controls/ServerInfoTreeView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace DBison.WPF.Controls
 {
@@ -13,5 +14,26 @@ namespace DBison.WPF.Controls
         }
 
         public TreeView TreeView => ThisTreeView;
+
+        private void __PreviewMouseButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is DependencyObject dependency)
+            {
+                TreeViewItem treeViewItem = __GetTreeViewItem(dependency);
+                if (treeViewItem != null)
+                {
+                    treeViewItem.IsSelected = true;
+                }
+            }
+        }
+
+        private TreeViewItem __GetTreeViewItem(DependencyObject source)
+        {
+            while (source is not null and not TreeViewItem)
+                source = System.Windows.Media.VisualTreeHelper.GetParent(source);
+
+            return source as TreeViewItem;
+        }
+
     }
 }
