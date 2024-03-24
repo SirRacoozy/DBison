fc887aef022ad3daaa7d3827e8d91a40413248fc
SirRacoozy <43681454+SirRacoozy@users.noreply.github.com>
2024-03-24 18:54:27 +0100

    Added `IConnectParsingPlugin`

---
+++ DBison.Plugin/IConnectParsingPlugin.cs
@@ -0,0 +1,12 @@
+﻿using DBison.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBison.Plugin;
+public interface IConnectParsingPlugin : IPlugin
+{
+    DatabaseInfo? ParseConnectInput(string connectInput);
+}
