2380b912b1cf9dcb241042297a9e35cd8d6db35c
timo <77628993+timi992000@users.noreply.github.com>
2024-04-02 17:24:50 +0200

    Begin of editor syntax

--- DBison.WPF/Controls/LineNumberedTextBox.cs
+++ DBison.WPF/Controls/LineNumberedTextBox.cs
@@ -1,16 +1,32 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace DBison.WPF.Controls
 {
-    public class LineNumberedTextBox : TextBox
+    public class LineNumberedTextBox : RichTextBox
     {
         #region - ctor -
         static LineNumberedTextBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(LineNumberedTextBox), new FrameworkPropertyMetadata(typeof(LineNumberedTextBox)));
         }
+
+        public LineNumberedTextBox()
+        {
+            Document.Blocks.Clear();
+        }
         #endregion
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(LineNumberedTextBox)/*, new PropertyMetadata(string.Empty, __TextChanged)*/);
 
         #region [OnApplyTemplate]
         public override void OnApplyTemplate()
@@ -24,7 +40,7 @@ namespace DBison.WPF.Controls
         private void __UpdateLineNumber()
         {
             if (GetTemplateChild("PART_LineNumberTextBlock") is TextBlock lineNumberTextBlock)
-                lineNumberTextBlock.Text = string.Join("\n", Enumerable.Range(1, Text.Split('\n').Length));
+                lineNumberTextBlock.Text = string.Join("\n", Enumerable.Range(1, __GetText().Split('\n').Length));
         }
         #endregion
 
@@ -35,5 +51,13 @@ namespace DBison.WPF.Controls
             __UpdateLineNumber();
         }
         #endregion
+
+        #region [__GetText]
+        private string __GetText()
+        {
+            return new TextRange(Document.ContentStart, Document.ContentEnd).Text;
+        }
+        #endregion
+
     }
 }
